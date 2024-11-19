c761a32c9df936639169d86bf3c7c1d6808e35f2
yakintech <cagatayyildiz87@gmail.com>
2024-11-19 15:59:17 +0300

    Update database connection string and add list of cities in Turkey

--- config/db.js
+++ config/db.js
@@ -5,8 +5,7 @@ const { default: mongoose } = require("mongoose");
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-
+        const conn = await mongoose.connect("mongodb+srv://shot-app-user:Cx2PMkjKYVBalYFt@cluster0.yk1lz.mongodb.net/shot-app-api");
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
